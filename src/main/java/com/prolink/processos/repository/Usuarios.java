a7eb2f1f9916fa3872211d2d65f0d5efe25f5e96
Tiago Souza <tiagoice@hotmail.com>
2019-06-11 17:28:20 -0300

    Usuarios Repositorio, metodo listarProtEntrada
    Listar usuários com pendencias de protocolos de entrada, sera devolvido a lista para tratamento.

--- src/main/java/com/prolink/processos/repository/Usuarios.java
+++ src/main/java/com/prolink/processos/repository/Usuarios.java
@@ -7,8 +7,13 @@ import com.prolink.processos.model.Usuario;
 
 @Repository
 public interface Usuarios extends JpaRepository<Usuario, Long>{
-	@Query(value = "SELECT * FROM CLIENTE c WHERE c.NOME like %?1", nativeQuery = true)
-    public List<Usuario> listarUsuariosProtocolosPendentes(){
-		
-	}
+	
+	@Query(value = "select distinct(u.*) from usuario as u 
+		inner join protocolo_entrada as e 
+		on e.para_id=u.id or e.recebido_id=u.id
+		where e.recebido=false and u.ativo=1 or e.devolver=true 
+		and e.devolvido=false u.ativo=1
+		order by u.nome", 
+	       nativeQuery = true)
+    	public List<Usuario> listarUsuariosProtocolosPendentes();
 }
